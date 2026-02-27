879efcc6bbc8c8063f991ef3ae4da7eca94cc735
alejandrapereaf <alejandrapereaf@gmail.com>
2026-02-27 11:52:19 +0100

    Update astro.config.mjs
    editado

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://astro.alejandrapereaf.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
